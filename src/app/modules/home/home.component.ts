15da7e21fca5349cc6618b31af7acc0553da30f8
Kevin Zambrano <kazt1995@gmail.com>
2024-06-26 02:28:08 -0500

    scroll infinito en section trades, section experience

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, AfterViewInit {
+  
+  @ViewChild('scroll1', { static: true }) scrollContainer: ElementRef | undefined;
+  items = ['Demolition', 'Civil', 'Earthwork','Demolition', 'Civil', 'Earthwork','Demolition', 'Civil', 'Earthwork','Demolition', 'Civil', 'Earthwork','Demolition', 'Civil', 'Earthwork','Demolition', 'Civil', 'Earthwork'];
+
+  
+  ngAfterViewInit(): void {
+    this.initScroll();
+  }
+  ngOnInit(): void {
+    
+  }
+
+  handleScroll() {
+    const container = this.scrollContainer?.nativeElement;
+    const scrollHeight = container.scrollHeight / 2;
+    if (container.scrollTop >= scrollHeight) {
+      container.scrollTop = 1;
+    } else if (container.scrollTop <= 0) {
+      container.scrollTop = scrollHeight - 1;
+    }
+  }
+
+  initScroll() {
+    const container = this.scrollContainer?.nativeElement;
+    const scrollHeight = container.scrollHeight / 2;
+    container.scrollTop = scrollHeight / 1; // Inicializa el scroll en el centro
+  }
 
 }
