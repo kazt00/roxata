65a964b37da2ac4c455801144f086f8399b5c13e
Kevin Zambrano <kazt1995@gmail.com>
2024-07-07 14:37:48 -0500

    ajuste de navbarFull y scrollToSection

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { AfterViewInit, Component, ElementRef, HostListener, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-navbar',
@@ -7,70 +8,77 @@ import { NavigationEnd, Router } from '@angular/router';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  isFullscreen = true;
-  isNavbarCollapsed = true;
-  private scrollThreshold: number = 100; // Ajusta este valor según lo necesario
 
   constructor(private router: Router, private elementRef: ElementRef) {
     
   }
 
   ngOnInit(): void {
-    this.toggleNavbarFull();
-    const navbarMin = document.getElementById('navbarMin');
-    if (navbarMin) {
-      navbarMin.classList.add('d-none');
-    }
-  }
-
-  scrollToSection(sectionId: string) {
-    this.router.navigate(['/']).then(() => {
-      const element = document.getElementById(sectionId);
-      if (element) {
-        element.scrollIntoView({ behavior: 'smooth', block: 'start' });
-      }
-    });
+    this.checkScrollPosition();
   }
 
   @HostListener('window:scroll', ['$event'])
   onWindowScroll() {
-    const navbar = document.getElementById('mainNavbar');
-    const navbarFull = document.getElementById('navbarFull');
-    const navbarMin = document.getElementById('navbarMin');
-    const body = document.body;
+    this.checkScrollPosition();
+  }
 
-    if (navbar && navbarFull && navbarMin) {
-      if (window.pageYOffset > this.scrollThreshold) {
-        navbar.classList.add('shrink', 'fixed-top');
-        body.classList.add('shrink-padding');
-        navbarMin.classList.remove('d-none');
-      } else {
-        navbar.classList.remove('shrink', 'fixed-top');
-        body.classList.remove('shrink-padding');
-        navbarMin.classList.add('d-none');
-      }
+  checkScrollPosition() {
+    if (window.scrollY === 0) { // Posición más alta de la página
+        this.showNavbarFull();
+    } else {
+        this.hideNavbarFull();
     }
   }
 
   scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
+  
+  scrollToSection(sectionId: string) {
+    this.hideNavbarFull();
+    setTimeout(() => {
+      const section = document.getElementById(sectionId);
+      const navbarMin = document.getElementById('navbarMin');
+      
+      if (section && navbarMin) {
+          const navbarMinHeight = navbarMin.offsetHeight;
 
-  private toggleNavbarFull() {
+          // Obtener la posición del elemento objetivo
+          const sectionPosition = section.getBoundingClientRect().top + window.pageYOffset;
+
+          // Calcular la posición ajustada
+          const adjustedPosition = sectionPosition - navbarMinHeight;
+
+          // Desplazarse a la posición ajustada
+          window.scrollTo({ top: adjustedPosition, behavior: 'smooth' });
+      }
+  }, 300);
+  }
+
+  hideNavbarFull() {
     const navbar = document.getElementById('mainNavbar');
     const navbarFull = document.getElementById('navbarFull');
+    const navbarMin = document.getElementById('navbarMin');
     const body = document.body;
+    if (navbar && navbarFull && navbarMin) {
+      navbar.classList.add('shrink');
+      body.classList.add('shrink-padding');
+      navbarFull.classList.add('d-none');
+      navbarMin.classList.remove('d-none');
+      navbarMin.classList.add('fixed-top');
+    }
+  }
 
-    if (navbar && navbarFull) {
-      if (window.pageYOffset > this.scrollThreshold) {
-        navbar.classList.add('shrink');
-        body.classList.add('shrink-padding');
-        navbarFull.classList.add('d-none');
-      } else {
-        navbar.classList.remove('shrink');
-        body.classList.remove('shrink-padding');
-        navbarFull.classList.remove('d-none');
-      }
+  showNavbarFull() {
+    const navbar = document.getElementById('mainNavbar');
+    const navbarFull = document.getElementById('navbarFull');
+    const navbarMin = document.getElementById('navbarMin');
+    const body = document.body;
+    if (navbar && navbarMin && navbarFull) {
+      navbar.classList.remove('shrink', 'fixed-top');
+      body.classList.remove('shrink-padding');
+      navbarMin.classList.add('d-none');
+      navbarFull.classList.remove('d-none');
     }
   }
 }
